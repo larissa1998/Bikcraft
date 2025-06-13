5b511c35760cd1072ab8f0fd428303e1784bc054
Larissa <larissafogacaavare@hotmail.com>
2025-06-13 19:44:36 -0300

    dom manipulate

---
+++ js/script.js
@@ -0,0 +1,60 @@
+//Ativar nav
+const links = document.querySelectorAll(".header-menu a");
+
+function ativar(link){
+  const url = location.href;
+  const href = link.href;
+  
+  if(url.includes(href)){
+      link.classList.add("ativo");
+  }
+}
+
+links.forEach(ativar);
+
+//Ativar tipos bike
+const parametros = new URLSearchParams(location.search);
+
+function ativarTipo(parametro){
+  const elemento = document.getElementById(parametro);
+  elemento.checked = true;
+}
+
+parametros.forEach(ativarTipo);
+
+//Perguntas frequentes
+const perguntas = document.querySelectorAll(".perguntas button");
+
+function ativarPergunta(event){
+  const pergunta = event.currentTarget;
+  const controls = pergunta.getAttribute('aria-controls');
+  const resposta = document.getElementById(controls);
+  
+  resposta.classList.toggle("ativa");
+  const ativa = resposta.classList.contains("ativa");
+  pergunta.setAttribute('aria-expanded', ativa); 
+}
+
+function eventosPerguntas(pergunta){
+  pergunta.addEventListener("click", ativarPergunta);
+}
+
+perguntas.forEach(eventosPerguntas);
+
+// Galeria de Bicicletas
+const galeria = document.querySelectorAll(".bicicleta-imagens img");
+const galeriaContainer = document.querySelector('bicicleta-imagens');
+
+function trocarImagem(event){
+  const img = event.currentTarget;
+  const media = matchMedia('(min-width: 1000px)').matches;
+  if(media){
+    galeriaContainer.prepend(img);
+  }
+}
+
+function eventosGaleria(img){
+  img.addEventListener('click', trocarImagem);
+}
+
+galeria.forEach(eventosGaleria);
